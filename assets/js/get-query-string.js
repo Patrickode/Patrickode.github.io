b6f7cd5c70684d88d01ac957026efd72802d2b71
Patrickode <prm5983@rit.edu>
2021-04-02 17:35:41 -0400

    Tidied up query string handling

--- assets/js/get-query-string.js
+++ assets/js/get-query-string.js
@@ -1,15 +1,18 @@
 "use strict";
 
-let resumes = [
+const resumes = [
     { name: "default", code: "1hYX119ocKAYXRaFw7V9kkYdolafDP59u" }
 ];
+Object.freeze(resumes);
 
 window.addEventListener("load", init);
 
 function init() {
+    //Split the url at the first ? and grab the second part
     let queryString = window.location.href.split("?", 2)[1];
     if (queryString) {
         for (let i = 0; i < resumes.length; i++) {
+            //If the query string matches this resume name, replace the website resume with this one and end the loop
             if (queryString.includes(`r=${resumes[i].name}`)) {
                 replaceResume(resumes[i].code);
                 break;
@@ -19,12 +22,18 @@ function init() {
 }
 
 function replaceResume(code) {
-    let buttonCode = document.querySelector("#resume-button").getAttribute("href");
-    let embedCode = document.querySelector("#resume-iframe").getAttribute("src");
-    if (buttonCode) {
-        buttonCode.replace(`${resumes[0].code}`, code);
-    }
-    if (embedCode) {
-        embedCode.replace(`${resumes[0].code}`, code);
-    }
+    updateResumeElement("#resume-button", "href", code);
+    updateResumeElement("#resume-iframe", "src", code);
+}
+
+function updateResumeElement(selector, attribute, code) {
+    //Get the element found with selector and its attribute entitled attribute, end early if either aren't found
+    let elemWithCode = document.querySelector(selector);
+    if (!elemWithCode) { return; }
+    let attribWithCode = elemWithCode.getAttribute(attribute);
+    if (!attribWithCode) { return; }
+
+    //Replace the default resume code with the supplied code
+    attribWithCode = attribWithCode.replace(`${resumes[0].code}`, code);
+    elemWithCode.setAttribute(attribute, attribWithCode);
 }
