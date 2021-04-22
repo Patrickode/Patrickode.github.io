c6cf89f3397def6143696155884a316ac3045684
Patrickode <prm5983@rit.edu>
2021-04-22 17:39:46 -0400

    Created proxy server offsite to get itch api data securely

--- assets/js/other-projects.js
+++ assets/js/other-projects.js
@@ -25,7 +25,7 @@ function sendItchXHR() {
     xhr.onerror = () => console.log("XHR network failure, other project grid will remain empty");
     xhr.onload = populateOtherProjectsGrid;
     //Send the request
-    xhr.open("GET", "https://itch.io/api/1/KEY_GOES_HERE/my-games");
+    xhr.open("GET", "https://people.rit.edu/prm5983/itch-proxy-for-portfolio.php");
     xhr.send();
 }
 
@@ -33,29 +33,25 @@ function populateOtherProjectsGrid(e) {
     let response = e.target;
     let results = JSON.parse(response.responseText);
 
-    if (results.games) {
-        results = results.games;
+    //Sort the results by their date, most recent first
+    results.sort((a, b) => {
+        let aDate = new Date(a.published_at);
+        let bDate = new Date(b.published_at);
+        return bDate - aDate;
+    });
+    //slice off any results past the 12th
+    results = results.length > 12 ? results.slice(0, 12) : results;
 
-        //Sort the results by their date, most recent first
-        results.sort((a, b) => {
-            let aDate = new Date(a.published_at);
-            let bDate = new Date(b.published_at);
-            return bDate - aDate;
-        });
-        //slice off any results past the 12th
-        results = results.length > 12 ? results.slice(0, 12) : results;
-
-        let grid = document.querySelector("#other-project-grid");
-        grid.innerHTML = "";
-        for (const result of results) {
-            grid.innerHTML += `<a href="${result.url}" class="other-project">
-                <img src="${result.cover_url}" alt="">
-                <h5>${result.title}</h5>
-                <h6>${result.short_text}</h6>
-            </a>`;
-        }
-
-        localStorage.itchXHRDate = new Date().setHours(0, 0, 0, 0);
-        localStorage.itchXHRContents = grid.innerHTML;
+    let grid = document.querySelector("#other-project-grid");
+    grid.innerHTML = "";
+    for (const result of results) {
+        grid.innerHTML += `<a href="${result.url}" class="other-project">
+            <img src="${result.cover_url}" alt="">
+            <h5>${result.title}</h5>
+            <h6>${result.short_text}</h6>
+        </a>`;
     }
+
+    localStorage.itchXHRDate = new Date().setHours(0, 0, 0, 0);
+    localStorage.itchXHRContents = grid.innerHTML;
 }
