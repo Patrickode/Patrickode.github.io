d6876d6bdff1ab95da0c0c04488d2cc1d3625960
Patrick Mitchell (RIT Student) <prm5983@rit.edu>
2022-04-11 17:36:37 -0400

    Made it so images expand on click using a modal

--- assets/js/modal-imgs.js
+++ assets/js/modal-imgs.js
@@ -3,13 +3,36 @@
 // Method 1
 //Get all images with modal class or similar identifier
 //when any of these are clicked, append their src to the current url
-    //this will take the user to a page where they can view the img in full
-    //When the user clicks the exit button, cut the img's source from the URL
+//this will take the user to a page where they can view the img in full
+//When the user clicks the exit button, cut the img's source from the URL
 
 //End result: regardless of query strings, the exit button of any img will lead to wherever the user was last
 
 //Method 2
 //Make div with display none, but otherwise at the front of everything
 //Show overlay div when image is clicked, replace its content with the image that was clicked
-    //image is max-width: 100%, height = auto
+//image is max-width: 100%, height = auto
 //When close button is pressed, or any part of the overlay that isn't the image ic clicked, set the display of the div to none again
+
+let overlay;
+let container;
+let modalImgs;
+
+window.addEventListener("load", init);
+
+function init() {
+    overlay = document.querySelector("#modal-overlay");
+    modalImgs = document.querySelectorAll(".modal");
+
+    overlay.addEventListener("click", (evt) => {
+        if (evt.target == overlay)
+            overlay.removeAttribute("active");
+    })
+
+    modalImgs.forEach(moImg => {
+        moImg.addEventListener("click", () => {
+            overlay.setAttribute("active", "");
+            overlay.innerHTML = `<img class="expanded-img" src="${moImg.getAttribute("src")}" alt="${moImg.getAttribute("alt")}" />`;
+        });
+    });
+}
