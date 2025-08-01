53d64934cf8882b21f6c7b4afc07f7d04fc61bd0
Patrick (GitKraken) <mitchellpatrick667@gmail.com>
2025-08-01 17:36:44 -0400

    Order formats alphabetically, update Rockstar format

--- assets/custom-js/formats.js
+++ assets/custom-js/formats.js
@@ -33,7 +33,7 @@ export const formats = {
      * @updated 2023-9-26
      */
     default: { resumeID: "1earLmYYhSdNJdLAIvhX8dx3Uaobzty8C", features: null },
-    
+
     //#region Special/Dev Formats
     /**
      * Special format used for testing; will result in invalid resume URLs\
@@ -61,6 +61,165 @@ export const formats = {
     //#endregion
 
     /**
+     * @created 2024-4-16
+     * @updated N/A
+     */
+    "apl": {
+        resumeID: "1gZBaD3ibW71vqXAaALjXg8OEoQOPa24M", features: [
+            { id: "nasa-gpba", content: null },
+            { id: "taste-of-culture", content: null },
+            { id: "remember-wonder", content: null },
+        ]
+    },
+    /**
+     * @created 2024-3-19
+     * @updated N/A
+     */
+    "applovin": {
+        resumeID: "1dVCXOJ8-Z5NXqnUdKfbaYXDLrywdJ58s", features: null
+    },
+    /**
+     * @created 2021-4-27
+     * @updated N/A
+     */
+    "changeling": {
+        resumeID: "1ZyPJy2a4LXnooWwJhXeA-SSlaDCACC4G", features: null
+    },
+    /**
+     * @created 2022-2-28
+     * @updated 2022-2-28
+     */
+    "epicgames": {
+        resumeID: "1dYwGAgilw0cqN1pPJpA-ZtiJ-VjymPBr", features: [
+            { id: "changeling", content: null },
+            { id: "livewire-lifesaver", content: null },
+            { id: "vr-independent-study", content: null },
+            { id: "divinity", content: null },
+        ]
+    },
+    /**
+     * @created 2023-3-18
+     * @updated N/A
+     */
+    "gdc2023": {
+        resumeID: "1apJGHbuK6yFEbQRbZfqGPyK8QifoAm3u", features: null
+    },
+    /**
+     * @created 2024-7-30
+     * @updated N/A
+     */
+    "grumpyface": {
+        resumeID: "1pO-Z_ChlhbUD2QWvDfvDsISWZwHGw_JO", features: [
+            { id: "remember-wonder", content: null },
+            { id: "taste-of-culture", content: null },
+            { id: "nasa-gpba", content: null },
+            { id: "livewire-lifesaver", content: null },
+        ]
+    },
+    /**
+     * @created 2024-4-16
+     * @updated N/A
+     */
+    "hello-games": {
+        resumeID: "1XchEZrotDKKKEgzUV7PT2YX0JSPN37Z6", features: [
+            { id: "nasa-gpba", content: null },
+            { id: "remember-wonder", content: null },
+            { id: "livewire-lifesaver", content: null },
+        ]
+    },
+    /**
+     * @created 2025-1-1
+     * @updated 2025-3-18
+     */
+    "ibm": {
+        resumeID: "1V-nrlqD7tbo0uTK0oEf4UOWYraea14pQ", features: [
+            { id: "remember-wonder", content: null },
+            { id: "taste-of-culture", content: null },
+            { id: "nasa-gpba", content: null },
+        ]
+    },
+    /**
+     * @created 2023-11-15
+     * @updated N/A
+     */
+    "iron-galaxy": {
+        resumeID: "1tFd4VP1Tt_AjZtCVsMhLDnmmyghidosr", features: [
+            { id: "nasa-gpba", content: null },
+            { id: "remember-wonder", content: null },
+            { id: "taste-of-culture", content: null },
+            { id: "livewire-lifesaver", content: null },
+        ]
+    },
+    /**
+     * @created 2025-3-20
+     * @updated N/A
+     */
+    "medtelligent": {
+        resumeID: "1gIzZaXR9ULATM7ZBEq8ydWEinHVVJ3Gs", features: [
+            { id: "remember-wonder", content: null },
+            { id: "nasa-gpba", content: null },
+            { id: "livewire-lifesaver", content: null },
+        ]
+    },
+    /**
+     * @created 2024-3-19
+     * @updated 2024-4-16
+     */
+    "netflix": {
+        resumeID: "1_MblCRB2f3258OGBP3M-NtX6UaZgC19d", features: [
+            { id: "remember-wonder", content: null },
+            { id: "nasa-gpba", content: null },
+            { id: "taste-of-culture", content: null },
+        ]
+    },
+    /**
+     * @created 2024-1-12
+     * @updated N/A
+     */
+    "nextgen": {
+        resumeID: "1CGg2CypCV7GSQKN28nYDMqVUVi3RjM3L", features: [
+            { id: "nasa-gpba", content: null },
+            { id: "taste-of-culture", content: null },
+            { id: "livewire-lifesaver", content: null },
+            { id: "remember-wonder", content: null },
+        ]
+    },
+    /**
+     * @created 2025-1-1
+     * @updated N/A
+     */
+    "nintendo": {
+        resumeID: "1Zu2Mo1pdzmqCCPOkJM1A2iFYFm_xX_dW", features: [
+            { id: "remember-wonder", content: null },
+            { id: "nasa-gpba", content: null },
+            { id: "livewire-lifesaver", content: null },
+        ]
+    },
+    /**
+     * @created 2024-1-12
+     * @updated N/A
+     */
+    "respawn": {
+        resumeID: "1OT4py-FT6gUTALSS0LVWhoJxp9N1N8qn", features: [
+            { id: "remember-wonder", content: null },
+            { id: "changeling", content: null },
+            { id: "taste-of-culture", content: null },
+            { id: "livewire-lifesaver", content: null },
+        ]
+    },
+    /**
+     * @created 2024-2-01
+     * @updated 2025-8-01
+     */
+    "rockstar": {
+        resumeID: "15fJBLUqvL7gP1J-OVpMdkBJl4ZW512X8", features: [
+            { id: "remember-wonder", content: null },
+            { id: "taste-of-culture", content: null },
+            { id: "nasa-gpba", content: null },
+            { id: "livewire-lifesaver", content: null },
+        ]
+    },
+    /**
      * @created 2025-6-23
      * @updated N/A
      */
@@ -83,48 +242,14 @@ export const formats = {
         ]
     },
     /**
-     * @created 2025-3-20
-     * @updated N/A
+     * @created 2022-4-12
+     * @updated 2022-4-18
      */
-    "medtelligent": {
-        resumeID: "1gIzZaXR9ULATM7ZBEq8ydWEinHVVJ3Gs", features: [
-            { id: "remember-wonder", content: null },
-            { id: "nasa-gpba", content: null },
+    "thatdamngoat": {
+        resumeID: "1C8_CPWsFKx2K4GbutkKKHkIX3CqqLrI7", features: [
             { id: "livewire-lifesaver", content: null },
-        ]
-    },
-    /**
-     * @created 2025-1-1
-     * @updated N/A
-     */
-    "nintendo": {
-        resumeID: "1Zu2Mo1pdzmqCCPOkJM1A2iFYFm_xX_dW", features: [
-            { id: "remember-wonder", content: null },
-            { id: "nasa-gpba", content: null },
-            { id: "livewire-lifesaver", content: null },
-        ]
-    },
-    /**
-     * @created 2025-1-1
-     * @updated 2025-3-18
-     */
-    "ibm": {
-        resumeID: "1V-nrlqD7tbo0uTK0oEf4UOWYraea14pQ", features: [
-            { id: "remember-wonder", content: null },
-            { id: "taste-of-culture", content: null },
-            { id: "nasa-gpba", content: null },
-        ]
-    },
-    /**
-     * @created 2024-7-30
-     * @updated N/A
-     */
-    "grumpyface": {
-        resumeID: "1pO-Z_ChlhbUD2QWvDfvDsISWZwHGw_JO", features: [
-            { id: "remember-wonder", content: null },
-            { id: "taste-of-culture", content: null },
-            { id: "nasa-gpba", content: null },
-            { id: "livewire-lifesaver", content: null },
+            { id: "vr-independent-study", content: null },
+            { id: "changeling", content: null },
         ]
     },
     /**
@@ -139,81 +264,6 @@ export const formats = {
         ]
     },
     /**
-     * @created 2024-4-16
-     * @updated N/A
-     */
-    "hello-games": {
-        resumeID: "1XchEZrotDKKKEgzUV7PT2YX0JSPN37Z6", features: [
-            { id: "nasa-gpba", content: null },
-            { id: "remember-wonder", content: null },
-            { id: "livewire-lifesaver", content: null },
-        ]
-    },
-    /**
-     * @created 2024-4-16
-     * @updated N/A
-     */
-    "apl": {
-        resumeID: "1gZBaD3ibW71vqXAaALjXg8OEoQOPa24M", features: [
-            { id: "nasa-gpba", content: null },
-            { id: "taste-of-culture", content: null },
-            { id: "remember-wonder", content: null },
-        ]
-    },
-    /**
-     * @created 2024-3-19
-     * @updated 2024-4-16
-     */
-    "netflix": {
-        resumeID: "1_MblCRB2f3258OGBP3M-NtX6UaZgC19d", features: [
-            { id: "remember-wonder", content: null },
-            { id: "nasa-gpba", content: null },
-            { id: "taste-of-culture", content: null },
-        ]
-    },
-    /**
-     * @created 2024-3-19
-     * @updated N/A
-     */
-    "applovin": {
-        resumeID: "1dVCXOJ8-Z5NXqnUdKfbaYXDLrywdJ58s", features: null
-    },
-    /**
-     * @created 2024-2-01
-     * @updated 2024-4-16
-     */
-    "rockstar": {
-        resumeID: "15fJBLUqvL7gP1J-OVpMdkBJl4ZW512X8", features: [
-            { id: "remember-wonder", content: null },
-            { id: "nasa-gpba", content: null },
-            { id: "taste-of-culture", content: null },
-        ]
-    },
-    /**
-     * @created 2024-1-12
-     * @updated N/A
-     */
-    "respawn": {
-        resumeID: "1OT4py-FT6gUTALSS0LVWhoJxp9N1N8qn", features: [
-            { id: "remember-wonder", content: null },
-            { id: "changeling", content: null },
-            { id: "taste-of-culture", content: null },
-            { id: "livewire-lifesaver", content: null },
-        ]
-    },
-    /**
-     * @created 2024-1-12
-     * @updated N/A
-     */
-    "NextGen": {
-        resumeID: "1CGg2CypCV7GSQKN28nYDMqVUVi3RjM3L", features: [
-            { id: "nasa-gpba", content: null },
-            { id: "taste-of-culture", content: null },
-            { id: "livewire-lifesaver", content: null },
-            { id: "remember-wonder", content: null },
-        ]
-    },
-    /**
      * @created 2024-1-12
      * @updated N/A
      */
@@ -225,53 +275,6 @@ export const formats = {
             { id: "remember-wonder", content: null },
         ]
     },
-    /**
-     * @created 2023-11-15
-     * @updated N/A
-     */
-    "iron-galaxy": {
-        resumeID: "1tFd4VP1Tt_AjZtCVsMhLDnmmyghidosr", features: [
-            { id: "nasa-gpba", content: null },
-            { id: "remember-wonder", content: null },
-            { id: "taste-of-culture", content: null },
-            { id: "livewire-lifesaver", content: null },
-        ]
-    },
-    /**
-     * @created 2023-3-18
-     * @updated N/A
-     */
-    "gdc2023": {
-        resumeID: "1apJGHbuK6yFEbQRbZfqGPyK8QifoAm3u", features: null
-    },
-    /**
-     * @created 2022-4-12
-     * @updated 2022-4-18
-     */
-    "thatdamngoat": {
-        resumeID: "1C8_CPWsFKx2K4GbutkKKHkIX3CqqLrI7", features: [
-            { id: "livewire-lifesaver", content: null },
-            { id: "vr-independent-study", content: null },
-            { id: "changeling", content: null },
-        ]
-    },
-    /**
-     * @created 2022-2-28
-     * @updated 2022-2-28
-     */
-    "epicgames": {
-        resumeID: "1dYwGAgilw0cqN1pPJpA-ZtiJ-VjymPBr", features: [
-            { id: "changeling", content: null },
-            { id: "livewire-lifesaver", content: null },
-            { id: "vr-independent-study", content: null },
-            { id: "divinity", content: null },
-        ]
-    },
-    /**
-     * @created 2021-4-27
-     * @updated N/A
-     */
-    "changeling": { resumeID: "1ZyPJy2a4LXnooWwJhXeA-SSlaDCACC4G", features: null },
 
 };
 Object.freeze(formats);
